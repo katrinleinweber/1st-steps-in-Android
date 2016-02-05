2299518b8e7cf78891bd27af6140455726a76d24
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-05 22:28:10 +0100

    remove settings menu

--- 01-HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ 01-HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.helloworld;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,27 +15,5 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         TextView helloText = (TextView) findViewById(R.id.myTextView);  // named variable = TextView cast from retrieved ID
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
