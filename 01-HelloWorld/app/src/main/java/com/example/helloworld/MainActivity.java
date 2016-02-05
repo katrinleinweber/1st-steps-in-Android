87d8dc008359b16f2d725fc86d4c7c0ff4bdf3a7
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-05 22:31:48 +0100

    remove tool-/action-/app-bar

--- 01-HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ 01-HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.helloworld;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         TextView helloText = (TextView) findViewById(R.id.myTextView);  // named variable = TextView cast from retrieved ID
     }
