38a3a10bfc6c4d9b2b852901298c70d03f6354e7
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-08 21:48:01 +0100

    bind EditText & add listener with callback

--- 03-Listeners/app/src/main/java/com/example/listeners/MainActivity.java
+++ 03-Listeners/app/src/main/java/com/example/listeners/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.listeners;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // declare class members
+    private EditText toToastText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toToastText = (EditText) findViewById(R.id.etToToast);
+
+        // initiate onClick callback for EditText only, but not yet for buttons
+        toToastText.setOnClickListener(new TextOnClickListener());
+    }
+
+    private class TextOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) { // receive clicked view as parameter
+
+        }
     }
 }
