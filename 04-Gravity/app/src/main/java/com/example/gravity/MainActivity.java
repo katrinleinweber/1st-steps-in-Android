8aa299cc4a3cb2b4b57f8da5e6f45ab064a71e7e
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-09 21:38:36 +0100

    demo padding (within view; to content)

--- 04-Gravity/app/src/main/java/com/example/gravity/MainActivity.java
+++ 04-Gravity/app/src/main/java/com/example/gravity/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // setContentView(R.layout.activity_main_margin);
+        // setContentView(R.layout.activity_main_padding);
     }
 }
