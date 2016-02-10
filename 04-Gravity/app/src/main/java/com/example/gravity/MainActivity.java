43f64804540b03fe1017bb5496a8d067219b54f3
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-10 19:12:19 +0100

    start conversion to tabbed view

--- 04-Gravity/app/src/main/java/com/example/gravity/MainActivity.java
+++ 04-Gravity/app/src/main/java/com/example/gravity/MainActivity.java
@@ -2,15 +2,24 @@ package com.example.gravity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // [ ] create buttons or tabs to switch between views
+
+    private Button showMargin;
+    private Button showPadding;
+    private Button showGravity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // setContentView(R.layout.activity_main_margin);
-        // setContentView(R.layout.activity_main_padding);
-        // setContentView(R.layout.activity_main_gravity);
+        setContentView(R.layout.activity_main);
+
+        // setContentView(R.layout.activity_main_margin);btnMrgn
+        // setContentView(R.layout.activity_main_padding);btnPddng
+        // setContentView(R.layout.activity_main_gravity);btnGrvt
     }
 }
