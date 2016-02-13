e3bc18bf0231975866b4daca9eb1ddc74bab123b
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-13 20:16:00 +0100

    prepare implicitIntent

--- 07-Intents/app/src/main/java/com/example/intents/SecondActivity.java
+++ 07-Intents/app/src/main/java/com/example/intents/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.intents;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
@@ -16,4 +17,9 @@ public class SecondActivity extends AppCompatActivity {
         Toast.makeText(this, bundleString, Toast.LENGTH_SHORT).show();
     }
 
+    public void onButtonClick(View view) {
+        Intent implicitIntent = new Intent();
+        implicitIntent.setAction(Intent.ACTION_DIAL);
+        startActivity(implicitIntent);
+    }
 }
