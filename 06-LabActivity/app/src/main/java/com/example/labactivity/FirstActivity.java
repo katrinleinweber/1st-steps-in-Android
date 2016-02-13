d8d74bd1aadd2706f5f29df711e02bbce0ff14c8
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-13 15:28:32 +0100

    insert other log messages
    tested & found working

--- 06-LabActivity/app/src/main/java/com/example/labactivity/FirstActivity.java
+++ 06-LabActivity/app/src/main/java/com/example/labactivity/FirstActivity.java
@@ -6,7 +6,7 @@ import android.util.Log;
 
 public class FirstActivity extends AppCompatActivity {
 
-    private static final String TAG = "FirstActivity";
+    private static final String TAG = "FirstActivity"; // FirstActivity.class.toString() automates & expands class string
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +16,43 @@ public class FirstActivity extends AppCompatActivity {
         Log.d(TAG, "I'm in onCreate() right now!");
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        setContentView(R.layout.activity_first);
+
+        Log.d(TAG, "I'm in onStart() right now!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_first);
+
+        Log.d(TAG, "I'm in onResume() right now!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setContentView(R.layout.activity_first);
+
+        Log.d(TAG, "I'm in onPause() right now!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_first);
+
+        Log.d(TAG, "I'm in onStop() right now!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        setContentView(R.layout.activity_first);
+
+        Log.d(TAG, "I'm in onDestroy() right now!");
+    }
 }
