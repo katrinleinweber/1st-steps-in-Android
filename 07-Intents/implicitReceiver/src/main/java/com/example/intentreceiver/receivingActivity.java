b6d4a487db0210c6ff19013410cf5242af235895
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-13 20:39:09 +0100

    attempt implicit intent to call back

--- 07-Intents/implicitReceiver/src/main/java/com/example/intentreceiver/receivingActivity.java
+++ 07-Intents/implicitReceiver/src/main/java/com/example/intentreceiver/receivingActivity.java
@@ -1,7 +1,9 @@
 package com.example.implicitreceiver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class receivingActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class receivingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiving);
+    }
+
+    public void callBack(View view) {
+        Intent callBack = new Intent();
+        callBack.putExtra(Intent.EXTRA_TEXT, getString(R.string.reply));
+        startActivity(callBack);
     }
 }
