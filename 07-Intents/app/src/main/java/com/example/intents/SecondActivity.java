160d8d49feb6bf15f837c9ad4a64a264987d6635
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-13 19:26:13 +0100

    start 2nd activity & transfer a string

--- 07-Intents/app/src/main/java/com/example/intents/SecondActivity.java
+++ 07-Intents/app/src/main/java/com/example/intents/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.intents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,6 +11,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String bundleString = getIntent().getExtras().getString(Intent.EXTRA_TEXT);
+        Toast.makeText(this, bundleString, Toast.LENGTH_SHORT).show();
     }
 
 }
