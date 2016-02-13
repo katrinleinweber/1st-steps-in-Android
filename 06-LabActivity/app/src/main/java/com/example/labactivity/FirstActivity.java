3eb3f116fe0e09b87df07782e6a5a395d1330617
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-13 15:17:51 +0100

    insert 1st log message after defining TAG constant

--- 06-LabActivity/app/src/main/java/com/example/labactivity/FirstActivity.java
+++ 06-LabActivity/app/src/main/java/com/example/labactivity/FirstActivity.java
@@ -2,13 +2,18 @@ package com.example.labactivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private static final String TAG = "FirstActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Log.d(TAG, "I'm in onCreate() right now!");
     }
 
 }
