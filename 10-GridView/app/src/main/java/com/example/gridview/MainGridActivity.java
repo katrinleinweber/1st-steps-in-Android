43439291379252ea5eabb0af5101f174d6194d46
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-14 20:51:54 +0100

    add 2-column grid of items

--- 10-GridView/app/src/main/java/com/example/gridview/MainGridActivity.java
+++ 10-GridView/app/src/main/java/com/example/gridview/MainGridActivity.java
@@ -2,6 +2,8 @@ package com.example.gridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainGridActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainGridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_grid);
+
+        GridView mainGrid = (GridView) findViewById(R.id.gvMainGrid);
+        mainGrid.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.list_objects_solar_system)));
     }
 }
