fae68f8cc7a26b2fa65d0beaccd457c33d2961bc
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-14 12:44:45 +0100

    connect activities via buttons & intents

--- 08-LabIntents/app/src/main/java/com/example/labintents/FirstActivity.java
+++ 08-LabIntents/app/src/main/java/com/example/labintents/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.example.labintents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void forwardTo2nd (View v) {
+        Intent forwardIntent = new Intent(FirstActivity.this, SecondActivity.class);
+        startActivity(forwardIntent);
     }
 }
--- 08-LabIntents/app/src/main/java/com/example/labintents/SecondActivity.java
+++ 08-LabIntents/app/src/main/java/com/example/labintents/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.labintents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void backTo1st(View v) {
+        Intent backTo1st = new Intent(SecondActivity.this, FirstActivity.class);
+        startActivity(backTo1st);
     }
 }
