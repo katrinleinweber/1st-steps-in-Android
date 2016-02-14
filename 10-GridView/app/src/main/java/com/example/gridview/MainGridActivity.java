46a7f333b8f064071c220af42c138cda7d4c3c42
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-14 20:52:50 +0100

    toast clicked item

--- 10-GridView/app/src/main/java/com/example/gridview/MainGridActivity.java
+++ 10-GridView/app/src/main/java/com/example/gridview/MainGridActivity.java
@@ -2,8 +2,11 @@ package com.example.gridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainGridActivity extends AppCompatActivity {
 
@@ -15,5 +18,13 @@ public class MainGridActivity extends AppCompatActivity {
         GridView mainGrid = (GridView) findViewById(R.id.gvMainGrid);
         mainGrid.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.list_objects_solar_system)));
+
+        mainGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(view.getContext(),
+                        (String) parent.getAdapter().getItem(position), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
