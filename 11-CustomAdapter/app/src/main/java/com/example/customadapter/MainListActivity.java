defed5cc0edd9b8ca5a50456e70ca4692c7429c1
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-16 20:26:01 +0100

    cast ListView from layout into controls

--- 11-CustomAdapter/app/src/main/java/com/example/customadapter/MainListActivity.java
+++ 11-CustomAdapter/app/src/main/java/com/example/customadapter/MainListActivity.java
@@ -2,6 +2,7 @@ package com.example.customadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainListActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        // cast ListView to variable countingList
+        ListView countingList = (ListView) findViewById(R.id.lvCounting);
     }
 }
