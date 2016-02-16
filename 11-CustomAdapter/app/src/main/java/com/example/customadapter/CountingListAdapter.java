c7bb466b687aebe8d57f6d5665ba63e7becbe4e8
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-16 20:34:42 +0100

    toast different strings for upper & lower half of list

--- 11-CustomAdapter/app/src/main/java/com/example/customadapter/CountingListAdapter.java
+++ 11-CustomAdapter/app/src/main/java/com/example/customadapter/CountingListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.customadapter;
 
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public class CountingListAdapter extends BaseAdapter {
@@ -18,4 +20,27 @@ public class CountingListAdapter extends BaseAdapter {
         return MAX_COUNT;
     }
 
+    @Override // callback returns different strings for toast, depending on item position
+    public Object getItem(int position) {
+        if (position > (MAX_COUNT/2)) {
+            return position + " down the list";
+        } else {
+            return "This is item: " + position;
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override // method
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) { // inflate .xml if view empty
+            convertView = mInflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        // search (inflated) convertView for IDs
+        ImageView image = (ImageView) convertView.findViewById(R.id.ivIcon);
+    }
 }
--- 11-CustomAdapter/app/src/main/java/com/example/customadapter/MainListActivity.java
+++ 11-CustomAdapter/app/src/main/java/com/example/customadapter/MainListActivity.java
@@ -2,8 +2,10 @@ package com.example.customadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainListActivity extends AppCompatActivity {
 
@@ -18,5 +20,13 @@ public class MainListActivity extends AppCompatActivity {
         // set new adapter of class CountingListAdapter to cast ListView
         countingList.setAdapter(new CountingListAdapter(getLayoutInflater()));
 
+        // also set ClickListener that constructs toast from adapter-provided data
+        countingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(view.getContext(), (String) parent.getAdapter().getItem(position),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
