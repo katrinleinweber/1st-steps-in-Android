11c9208ef95c92ff417398ee877793047c210721
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-16 20:29:06 +0100

    prepare LayoutInflater

--- 11-CustomAdapter/app/src/main/java/com/example/customadapter/CountingListAdapter.java
+++ 11-CustomAdapter/app/src/main/java/com/example/customadapter/CountingListAdapter.java
@@ -1,7 +1,21 @@
 package com.example.customadapter;
 
+import android.view.LayoutInflater;
 import android.widget.BaseAdapter;
 
 public class CountingListAdapter extends BaseAdapter {
+
+    private static final int MAX_COUNT = 36;
+    private LayoutInflater mInflater;
+
+    // constructor that receives LayoutInflater
+    public CountingListAdapter(LayoutInflater inflater) {
+        mInflater = inflater;
+    }
+
+    @Override // setting a constant
+    public int getCount() {
+        return MAX_COUNT;
+    }
 
 }
