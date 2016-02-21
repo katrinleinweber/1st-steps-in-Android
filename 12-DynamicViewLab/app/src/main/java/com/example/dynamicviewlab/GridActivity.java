6d0cd8567cb6d07de15f4cc16264ac489231f4b7
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-21 13:50:37 +0100

    create intent to GridActivity

--- 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/GridActivity.java
+++ 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/GridActivity.java
@@ -2,6 +2,7 @@ package com.example.dynamicviewlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class GridActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class GridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+
+        GridView gridView = (GridView) findViewById(R.id.gvItems);
     }
 }
--- 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/ListActivity.java
+++ 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/ListActivity.java
@@ -1,7 +1,9 @@
 package com.example.dynamicviewlab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -19,6 +21,12 @@ public class ListActivity extends AppCompatActivity {
         // set new instance of ListGridAdapter-class as adapter for listView
         listView.setAdapter(new ListGridAdapter(getLayoutInflater(),
                 ListGridAdapter.AdapterMode.LIST, MAX_LIST_ITEMS));
+
+        // create new intent to GridActivity-class within ClickListener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent gridIntent = new Intent(view.getContext(), GridActivity.class);
 
     }
 }
