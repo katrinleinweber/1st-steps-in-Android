413c30f5c58c19110e0045f67b2595c214709338
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-21 13:50:37 +0100

    alternate between icons in both activities

--- 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/GridActivity.java
+++ 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/GridActivity.java
@@ -3,6 +3,7 @@ package com.example.dynamicviewlab;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class GridActivity extends AppCompatActivity {
@@ -16,5 +17,7 @@ public class GridActivity extends AppCompatActivity {
         int maxItems = getIntent().getExtras().getInt(Intent.EXTRA_UID);
 
         GridView gridView = (GridView) findViewById(R.id.gvItems);
+        gridView.setAdapter(new ListGridAdapter(getLayoutInflater(),
+                ListGridAdapter.AdapterMode.GRID, maxItems));
     }
 }
--- 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/ListGridAdapter.java
+++ 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/ListGridAdapter.java
@@ -1,9 +1,19 @@
 package com.example.dynamicviewlab;
 
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ListGridAdapter extends BaseAdapter {
+
+    // enumeration with 2 values
+    public static enum AdapterMode {
+        LIST,
+        GRID
+    }
 
     private LayoutInflater mInflater;
     private AdapterMode mCurrentMode;
@@ -17,7 +27,43 @@ public class ListGridAdapter extends BaseAdapter {
     }
 
     @Override
+    public int getCount() {
+        return mMaxItems;
+    }
+
+    @Override
     public Object getItem(int position) {
         return new Integer(position + 1);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position + 1;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        boolean inListMode = mCurrentMode == AdapterMode.LIST;
+        boolean isEvenPos = position % 2 == 0;
+        if(convertView == null){
+            convertView = mInflater.inflate(inListMode?
+                    R.layout.list_item:R.layout.grid_item, parent, false);
+        }
+
+        ImageView icon = (ImageView) convertView.findViewById(R.id.ivIcon);
+
+        int resourceID;
+        if (inListMode) {
+            resourceID = isEvenPos?android.R.drawable.ic_lock_lock:android.R.drawable.ic_dialog_alert;
+        } else {
+            resourceID = isEvenPos?android.R.drawable.ic_menu_call:android.R.drawable.ic_menu_add;
+        } // test position for oddness & set different icons (syntax: testOnVariable?resource_if_yes:resource_if_no)
+
+        icon.setImageResource(resourceID);
+
+        TextView itemVal = (TextView) convertView.findViewById(R.id.tvValue);
+        itemVal.setText(String.valueOf(position + 1));
+
+        return convertView;
     }
 }
