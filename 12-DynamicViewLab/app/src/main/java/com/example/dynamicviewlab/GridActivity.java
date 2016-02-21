2b7de09f55faff827a9001797deb86774d834add
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-02-21 14:05:10 +0100

    toast item position

--- 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/GridActivity.java
+++ 12-DynamicViewLab/app/src/main/java/com/example/dynamicviewlab/GridActivity.java
@@ -3,8 +3,10 @@ package com.example.dynamicviewlab;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class GridActivity extends AppCompatActivity {
 
@@ -19,5 +21,14 @@ public class GridActivity extends AppCompatActivity {
         GridView gridView = (GridView) findViewById(R.id.gvItems);
         gridView.setAdapter(new ListGridAdapter(getLayoutInflater(),
                 ListGridAdapter.AdapterMode.GRID, maxItems));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(view.getContext(), parent.getAdapter().getItem(position).toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
